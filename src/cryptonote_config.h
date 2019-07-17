f37d36b428366af6a960aaedd1ec2db7010c8f83
StabCoin <52980628+StabCoin@users.noreply.github.com>
2019-07-17 10:48:09 +0000

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -75,8 +75,8 @@
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
 
 
-#define DIFFICULTY_TARGET_V2                            360  // seconds
-#define DIFFICULTY_TARGET_V1                            90  // seconds - before first fork
+#define DIFFICULTY_TARGET_V2                            240  // seconds
+#define DIFFICULTY_TARGET_V1                            120  // seconds - before first fork
 #define DIFFICULTY_WINDOW                               720 // blocks
 #define DIFFICULTY_LAG                                  15  // !!!
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
