4634905562fa5cf10276a33ce8a01e55e19aada3
StabCoin <52980628+StabCoin@users.noreply.github.com>
2019-07-18 00:10:49 +0000

    Update wallet_rpc_server.cpp

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -64,7 +64,7 @@ namespace
   const command_line::arg_descriptor<std::string> arg_wallet_dir = {"wallet-dir", "Directory for newly created wallets"};
   const command_line::arg_descriptor<bool> arg_prompt_for_password = {"prompt-for-password", "Prompts for password when not provided", false};
 
-  constexpr const char default_rpc_username[] = "aeon";
+  constexpr const char default_rpc_username[] = "stab";
 
   boost::optional<tools::password_container> password_prompter(const char *prompt, bool verify)
   {
@@ -218,7 +218,7 @@ namespace tools
           string_encoding::base64_encode(rand_128bit.data(), rand_128bit.size())
         );
 
-        std::string temp = "aeon-wallet-rpc." + bind_port + ".login";
+        std::string temp = "stab-wallet-rpc." + bind_port + ".login";
         rpc_login_file = tools::private_file::create(temp);
         if (!rpc_login_file.handle())
         {
@@ -652,7 +652,7 @@ namespace tools
           }
           if (addresses.empty())
           {
-            er.message = std::string("No Aeon address found at ") + url;
+            er.message = std::string("No Stab address found at ") + url;
             return {};
           }
           return addresses[0];
@@ -1717,7 +1717,7 @@ namespace tools
         }
         if (addresses.empty())
         {
-          er.message = std::string("No Aeon address found at ") + url;
+          er.message = std::string("No Stab address found at ") + url;
           return {};
         }
         return addresses[0];
@@ -2489,7 +2489,7 @@ namespace tools
         }
         if (addresses.empty())
         {
-          er.message = std::string("No Aeon address found at ") + url;
+          er.message = std::string("No Stab address found at ") + url;
           return {};
         }
         return addresses[0];
@@ -3289,12 +3289,12 @@ int main(int argc, char** argv) {
   bool should_terminate = false;
   std::tie(vm, should_terminate) = wallet_args::main(
     argc, argv,
-    "aeon-wallet-rpc [--wallet-file=<file>|--generate-from-json=<file>|--wallet-dir=<directory>] [--rpc-bind-port=<port>]",
-    tools::wallet_rpc_server::tr("This is the RPC Aeon wallet. It needs to connect to an Aeon\ndaemon to work correctly."),
+    "stab-wallet-rpc [--wallet-file=<file>|--generate-from-json=<file>|--wallet-dir=<directory>] [--rpc-bind-port=<port>]",
+    tools::wallet_rpc_server::tr("This is the RPC Stab wallet. It needs to connect to an Stab\ndaemon to work correctly."),
     desc_params,
     po::positional_options_description(),
     [](const std::string &s, bool emphasis){ epee::set_console_color(emphasis ? epee::console_color_white : epee::console_color_default, true); std::cout << s << std::endl; if (emphasis) epee::reset_console_color(); },
-    "aeon-wallet-rpc.log",
+    "stab-wallet-rpc.log",
     true
   );
   if (!vm)
